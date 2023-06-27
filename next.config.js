6be969b9074eb953ef3ef765eda74f8c41716b50
Jull Weber <jull@monkeykodeagency.com>
2023-06-27 13:37:27 -0700

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  output: 'export',
-  distDir: 'docs',
+  basePath: isProd ? '/jullweber/' : '',
+  assetPrefix: isProd ? '/jullweber/' : '',
 };
 
 module.exports = nextConfig;
