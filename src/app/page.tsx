2cecb076f305113ec1a267ec578cefb00981fc1d
Jull Weber <jull@monkeykodeagency.com>
2023-06-27 08:10:21 -0700

    wip: start adding bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Logo from '../components/Logo';
 import Link from 'next/link';
+import Image from 'next/image';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
 export default function Home() {
   return (
     <>
@@ -39,49 +47,85 @@ export default function Home() {
             </div>
           </section>
           <section className="text-left mt-18">
-            <h2>Bio +</h2>
-            <p>
-              I have over 20 years of experience in web and web app development,
-              specializing in transforming abstract ideas into innovative
-              solutions. I aim to create engaging, user-friendly applications
-              and online experiences that solve real-world problems.
-            </p>
-            <p>
-              My technical and software design skills were honed through a
-              combination of computer science and engineering programs at Purdue
-              University and a Bachelor of Science degree from MIT, which I
-              began pursuing at the young age of 11.
-            </p>
-            <p>
-              I further developed my expertise by working as a Tech Lead at
-              various advertising agencies in Los Angeles, where I created
-              numerous online user experiences for high-profile clients such as
-              Disney, ESPN, Honda, Turbotax, Dole, Nestlé, Lenovo laptops,
-              Qantas, Suzuki, Wachovia, and Wells Fargo.
-            </p>
-            <p>
-              I am the owner of Monkey Kode, a web development agency
-              specializing in E-commerce, Headless sites, Custom CMSs, and
-              complex API integrations. My team and I strive to deliver
-              innovative solutions that exceed clients' expectations.I have over
-              20 years of experience in web and web app development,
-              specializing in transforming abstract ideas into innovative
-              solutions. I aim to create engaging, user-friendly applications
-              and online experiences that solve real-world problems. My
-              technical and software design skills were honed through a
-              combination of computer science and engineering programs at Purdue
-              University and a Bachelor of Science degree from MIT, which I
-              began pursuing at the young age of 11. I further developed my
-              expertise by working as a Tech Lead at various advertising
-              agencies in Los Angeles, where I created numerous online user
-              experiences for high-profile clients such as Disney, ESPN, Honda,
-              Turbotax, Dole, Nestlé, Lenovo laptops, Qantas, Suzuki, Wachovia,
-              and Wells Fargo. I am the owner of Monkey Kode, a web development
-              agency specializing in E-commerce, Headless sites, Custom CMSs,
-              and complex API integrations. My team and I strive to deliver
-              innovative solutions that exceed clients' expectations.
-            </p>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  <h2>Bio +</h2>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                    <div>
+                      <p>
+                        I have over 20 years of experience in web and web app
+                        development, specializing in transforming abstract ideas
+                        into innovative solutions. I aim to create engaging,
+                        user-friendly applications and online experiences that
+                        solve real-world problems.
+                      </p>
+                      <p>
+                        My technical and software design skills were honed
+                        through a combination of computer science and
+                        engineering programs at Purdue University and a Bachelor
+                        of Science degree from MIT, which I began pursuing at
+                        the young age of 11.
+                      </p>
+                      <p>
+                        I further developed my expertise by working as a Tech
+                        Lead at various advertising agencies in Los Angeles,
+                        where I created numerous online user experiences for
+                        high-profile clients such as Disney, ESPN, Honda,
+                        Turbotax, Dole, Nestlé, Lenovo laptops, Qantas, Suzuki,
+                        Wachovia, and Wells Fargo.
+                      </p>
+                      <p>
+                        I am the owner of Monkey Kode, a web development agency
+                        specializing in E-commerce, Headless sites, Custom CMSs,
+                        and complex API integrations. My team and I strive to
+                        deliver innovative solutions that exceed clients&apos;
+                        expectations.I have over 20 years of experience in web
+                        and web app development, specializing in transforming
+                        abstract ideas into innovative solutions. I aim to
+                        create engaging, user-friendly applications and online
+                        experiences that solve real-world problems. My technical
+                        and software design skills were honed through a
+                        combination of computer science and engineering programs
+                        at Purdue University and a Bachelor of Science degree
+                        from MIT, which I began pursuing at the young age of 11.
+                        I further developed my expertise by working as a Tech
+                        Lead at various advertising agencies in Los Angeles,
+                        where I created numerous online user experiences for
+                        high-profile clients such as Disney, ESPN, Honda,
+                        Turbotax, Dole, Nestlé, Lenovo laptops, Qantas, Suzuki,
+                        Wachovia, and Wells Fargo. I am the owner of Monkey
+                        Kode, a web development agency specializing in
+                        E-commerce, Headless sites, Custom CMSs, and complex API
+                        integrations. My team and I strive to deliver innovative
+                        solutions that exceed clients&apos; expectations.
+                      </p>
+                    </div>
+                    <div
+                      style={
+                        {
+                          //   position: 'relative',
+                          //   width: '100%',
+                          //   height: '100%',
+                          //   minHeight: '572px',
+                        }
+                      }
+                    >
+                      <Image
+                        src={'/Jull-B2.png'}
+                        alt="Jull Weber"
+                        width={512}
+                        height={572}
+                      />
+                    </div>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </section>
+          <section></section>
         </div>
       </main>
     </>
